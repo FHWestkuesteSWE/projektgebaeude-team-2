77a44b989a5756f54bc486ca1d59808699fa02cd
rinaldy <rinaldysutyono@gmail.com>
2020-05-08 14:00:08 +0200

    test Raum class

--- Software/GebaeudeManagement/Server/main.cpp
+++ Software/GebaeudeManagement/Server/main.cpp
@@ -1,5 +1,6 @@
 #include "Server.h"
 #include "Raum.h"
+#include "UnitTest.h"
 
 #include <iostream>
 #include <string>
@@ -8,9 +9,13 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
+	UnitTest obj;
+	obj.runUnitTest();
+	
 
 	/* ---------------- SCHLAFZIMMER ---------------- */
 	// Define Raum und Fenster
+	/*
 	string fensterName[] = { "links", "mitte", "rechts" };
 	unsigned int numOfFenster = sizeof(fensterName) / sizeof(fensterName[0]);
 	string lampeName[] = { "LampTable", "LampLeft", "LampRight" , "LampDoor" };
@@ -37,17 +42,18 @@ int main(int argc, char* argv[]) {
 	cout << "number_of_lampe: " << schlafzimmer.getNumOfLampe() << endl;
 
 	cout << endl << endl;
-
+	*/
 
 
 	/* ---------------- KELLER ---------------- */
+	/*
 	string lampeNameKeller[] = { "Mitte" };
 	unsigned int numOfLampKeller = sizeof(lampeNameKeller) / sizeof(lampeNameKeller[0]);
 	Raum keller("Keller", NULL, 0, lampeNameKeller, numOfLampKeller);
 	cout << "RAUM: " << keller.getName() << endl;
 	cout << "number_of_fenster: " << keller.getNumOfFenster() << endl;
 	cout << "number_of_lampe: " << keller.getNumOfLampe() << endl;
-
+	*/
 
 	Server s;
 	s.start(argv[1]);
