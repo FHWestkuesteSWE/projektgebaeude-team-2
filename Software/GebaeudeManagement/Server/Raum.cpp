91fe11ebc1e45ba9783b0a4936b8a6c181462079
rinaldy <rinaldysutyono@gmail.com>
2020-05-08 13:59:19 +0200

    throw exception

--- Software/GebaeudeManagement/Server/Raum.cpp
+++ Software/GebaeudeManagement/Server/Raum.cpp
@@ -4,23 +4,15 @@
 
 using namespace std;
 
+
+Raum::Raum() {
+	;
+}
+
 Raum::Raum(string p_name, string fensterName[], int lengthFensterArr, string lampeName[], int lenghtLampArr) {
 	// Now array fensterName is decayed as pointer
 	// Because the array in parameter decayed as pointer, there is no change to get the length of it.
 	// It is automaticly became an address of the array
-
-
-	// Set Raum name
-	this->name = p_name;
-
-	// Allocate Fenster, Lampe object based on the given length parameter
-	this->fenster = new Fenster[lengthFensterArr];
-	this->lampe = new Lampe[lenghtLampArr];
-
-	// Save total of Fenster and Lampe in variable
-	this->numOfFenster = lengthFensterArr;
-	this->numOfLampe = lenghtLampArr;
-
 
 	/* ------------ FENSTER ------------ */
 	// Check if Raum has Fenster or not
@@ -29,15 +21,30 @@ Raum::Raum(string p_name, string fensterName[], int lengthFensterArr, string lam
 		cout << "Raum " << this->name << "does not have Fenster" << endl;
 	}
 	else {
-		// Raum has Fenster
-		for (int i = 0; i < lengthFensterArr; i++){
-
-			// Set Fenster name
-			this->fenster[i].setName(fensterName[i]);
+		if (lengthFensterArr == 0) {
+			throw "Error - Length of fensterName Array is 0, while fensterName Array is not empty";
 		}
+		else {
+			// Set Raum name
+			this->name = p_name;
 
+			// Allocate Fenster, Lampe object based on the given length parameter
+			this->fenster = new Fenster[lengthFensterArr];
+			this->lampe = new Lampe[lenghtLampArr];
+
+			// Save total of Fenster and Lampe in variable
+			this->numOfFenster = lengthFensterArr;
+			this->numOfLampe = lenghtLampArr;
+
+			// Raum has Fenster
+			for (int i = 0; i < lengthFensterArr; i++) {
+
+				// Set Fenster name
+				this->fenster[i].setName(fensterName[i]);
+			}
+
+		}
 	}
-
 
 	/* ------------ Lampe ------------ */
 	// Check if Raum has Lampe or not
@@ -46,15 +53,22 @@ Raum::Raum(string p_name, string fensterName[], int lengthFensterArr, string lam
 		cout << "Raum " << this->name << "does not have Lampe" << endl;
 	}
 	else {
-		// Raum has Lampe
-		for (int i = 0; i < lengthFensterArr; i++) {
-
-			// Set Lampe name
-			this->lampe[i].setName(lampeName[i]);
-
-			// Set Lampe state to off (false)
-			this->lampe[i].setState(false);
+		if (lenghtLampArr == 0) {
+			throw "Error - Length of lenghtLampArr is 0, while lampeName Array is not empty";
 		}
+		else {
+			// Raum has Lampe
+			for (int i = 0; i < lenghtLampArr; i++) {
+
+				// Set Lampe name
+				this->lampe[i].setName(lampeName[i]);
+
+				// Set Lampe state to off (false)
+				this->lampe[i].setState(false);
+			}
+
+		}
+
 
 	}
 
