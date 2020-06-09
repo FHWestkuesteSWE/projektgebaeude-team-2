077423e07103c4c5fd5671cf208d783a63ca7c37
rinaldy <rinaldysutyono@gmail.com>
2020-06-09 20:04:31 +0200

    add list of error code

---
+++ Software/GebaeudeManagement/Server/ErrorCode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#pragma once
+#include <string>
+
+using namespace std;
+
+#define SERVER_NO_ERROR "0"
+#define SENSOR_ACTOR_NOT_FOUND "1"
+#define ROOM_NOT_FOUND "2"
+#define ROOM_IS_EXISTED "3"
+#define SERVER_ERROR "100"
+
+
+
+
+
+
