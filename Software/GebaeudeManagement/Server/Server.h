ddf7330da2033bb32d8fda71601a2b857095658f
rinaldy <rinaldysutyono@gmail.com>
2020-06-09 20:06:49 +0200

    implement function for Create request

--- Software/GebaeudeManagement/Server/Server.h
+++ Software/GebaeudeManagement/Server/Server.h
@@ -2,10 +2,14 @@
 #include <string>
 #include <vector>
 #include "BasicServer.h"
+#include "Raum.h"
 
 using namespace std;
 
 class Server : BasicServer {
+private:
+	Raum* raumObj;
+
 public:
 	void start(char port[]);
 	Server();
@@ -16,11 +20,12 @@ public:
 	void getActorFromRoom(string roomname, char* actor);
 	void getAllRooms(char* rooms);
 
-	// todo
 	void getSensorState(string roomname, string sensorname, char* ans);
 	void setActorState(string roomname, string actorname, string action, char* ans);
+	void createRoom(string p_name, string fensterName[], int lengthFensterArr, string lampeName[], int lenghtLampArr, char* ans);
 
 	void prepareAnswer(string* content, int content_len, char* ans);
+	void getPropertiesFromMessage(string raw, int propertes_len, string* res);
 
 
 protected:
